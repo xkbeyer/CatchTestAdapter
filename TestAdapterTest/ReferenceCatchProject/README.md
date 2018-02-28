5bb072947d97303bc06a73b3d3899571db64e603
Oski Kervinen <oski.kervinen@m-files.com>
2018-02-28 10:18:54 +0200

    Add projects for tests and for producing sample catch output

---
+++ TestAdapterTest/ReferenceCatchProject/README.md
@@ -0,0 +1,3 @@
+# Reference Catch Project
+
+This project contains Catch tests and is meant to be a sample for testing the adapter.
---
+++ TestAdapterTest/TestParsing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestAdapterTest
+{
+	[TestClass]
+	public class TestParsing
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
