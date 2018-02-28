7cab572354b2407cc19a4ba7da4e976670cde848
Oski Kervinen <oski.kervinen@m-files.com>
2018-02-28 13:37:46 +0200

    Test the line numbers produced by the adapter

--- TestAdapterTest/TestTestDiscoverer.cs
+++ TestAdapterTest/TestTestDiscoverer.cs
@@ -15,6 +15,7 @@ namespace TestAdapterTest
         // The path to the reference catch executable.
         const string referenceExe = @"ReferenceCatchProject.exe";
         const string referenceExePath = @"..\..\..\x64\Debug\ReferenceCatchProject.exe";
+        readonly List<string> referenceExeList = new List<String>() { referenceExe };
 
         // Tests that all the tests in the reference project are found.
         [TestMethod]
@@ -26,7 +27,7 @@ namespace TestAdapterTest
 
             // Discover tests from the reference project.
             TestDiscoverer discoverer = new TestDiscoverer();
-            discoverer.DiscoverTests( new List<String>() { referenceExe },
+            discoverer.DiscoverTests( referenceExeList,
                 new MockDiscoveryContext(),
                 new MockMessageLogger(),
                 testSink );
@@ -35,6 +36,43 @@ namespace TestAdapterTest
             Assert.AreEqual( testSink.Tests.Count, 3 );
         }
 
+        // Tests that the test case lines are correct.
+        [TestMethod]
+        [DeploymentItem( referenceExePath )]
+        public void TestCaseLinesCorrect()
+        {
+            // Initialize a mock sink to keep track of the discovered tests.
+            MockTestCaseDiscoverySink testSink = new MockTestCaseDiscoverySink();
 
+            // Discover tests from the reference project.
+            TestDiscoverer discoverer = new TestDiscoverer();
+            discoverer.DiscoverTests( referenceExeList,
+                new MockDiscoveryContext(),
+                new MockMessageLogger(),
+                testSink );
+
+            // The reference project cases are on these lines.
+            // Currently, the line of the first section in the case is reported.
+            var linesOfCases = new Dictionary<string, int>();
+            linesOfCases.Add( "No tags", 8 );
+            linesOfCases.Add( "With tags", 18 );
+            linesOfCases.Add( "Has failure", 26 );
+
+            foreach( var test in testSink.Tests )
+            {
+                // Process only tests we have hard coded here.
+                if( linesOfCases.ContainsKey( test.FullyQualifiedName ) )
+                {
+                    // Check that the line number is correct.
+                    Assert.AreEqual( linesOfCases[ test.FullyQualifiedName ], test.LineNumber, test.FullyQualifiedName );
+
+                    // Remove the case so we can check all were handled.
+                    linesOfCases.Remove( test.FullyQualifiedName );
+                }
+            }
+
+            // Make sure all the cases we wanted got checked.
+            Assert.AreEqual( linesOfCases.Count, 0, String.Format( "Unhandled cases: {0}", linesOfCases.ToString() ) );
+        }
     }
 }
