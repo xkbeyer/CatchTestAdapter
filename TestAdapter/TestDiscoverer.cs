5b0102890dd633e32847a08304efd226f8592e53
Oski Kervinen <oski.kervinen@m-files.com>
2018-02-28 13:39:23 +0200

    Try to parse test cases from the list output
    The old way of parsing their locations by running them
    1) runs them, which we don't want
    2) Runs a new process for each test, which is slow.
    
    Discovering them all with one execution should be faster.

--- TestAdapter/TestDiscoverer.cs
+++ TestAdapter/TestDiscoverer.cs
@@ -5,6 +5,7 @@ using Microsoft.VisualStudio.TestWindow;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace CatchTestAdapter
 {
@@ -28,59 +29,86 @@ namespace CatchTestAdapter
         }
 
 
-        public static IList<TestCase> CreateTestCases(string exeName)
+        public static IList<TestCase> CreateTestCases( string exeName )
         {
             var testCases = new List<TestCase>();
-            var p = new ProcessRunner(exeName, "--list-test-names-only");
+            var p = new ProcessRunner(exeName, "--list-tests --verbosity high");
 
-            foreach (var line in p.Output)
+            foreach (var test in ParseListing( exeName, p.Output ) )
             {
-                testCases.Add(Generate(exeName, line));
+                testCases.Add( test );
             }
 
             return testCases;
         }
 
-        enum scan_state { init, deli, source };
+        // Regular expression for separating file path and line number from catch output.
+        static Regex lineInfoPattern = new Regex( @"(?<path>.*)\((?<line>\d+)\)" );
 
-        private static TestCase Generate(string exeName, string testName)
+        private static TestCase LineGroupToTestCase( string exeName, IList<string> lineGroup )
         {
-            var p = new ProcessRunner(exeName, "-s \"" + testName + "\"");
-            scan_state state = scan_state.init;
-            foreach (var line in p.Output)
+            // The group needs to have at least three lines.
+            if ( lineGroup.Count < 3 )
             {
-                switch (state)
-                {
-                    case scan_state.init:
-                        if (line == testName)
-                        {
-                            state = scan_state.deli;
-                        }
-                        break;
-                    case scan_state.deli:
-                        if (line.StartsWith("----"))
-                        {
-                            state = scan_state.source;
-                        }
-                        break;
-                    case scan_state.source:
-                        string pattern = @"\(\d+\)";
-                        Regex rgx = new Regex(pattern, RegexOptions.IgnoreCase);
-                        MatchCollection matches = rgx.Matches(line);
-                        string lineno = "";
-                        if (matches.Count != 0)
-                        {
-                            lineno = matches[0].Value.Trim('(', ')');
-                            string source = line.Remove(line.IndexOf('('));
-                            var test = new TestCase(testName, TestExecutor.ExecutorUri, exeName);
-                            test.CodeFilePath = source;
-                            test.LineNumber = Int32.Parse(lineno);
-                            return test;
-                        }
-                        break;
-                }
+                string lines = lineGroup.Aggregate( "", ( add, acc ) => add + "\n" + acc );
+                throw new Exception( string.Format( "Unexpectedly few lines in catch output group: '{0}'", lines ) );
             }
-            return null;
+
+            // Get the name.
+            string name = lineGroup[ 0 ].Trim();
+
+            // Parse line info with regex.
+            string lineInfoString = lineGroup[ 1 ].Trim();
+            var match = lineInfoPattern.Match( lineInfoString );
+            if ( !match.Success )
+                throw new Exception(String.Format("Could not parse line info from '{0}'.", lineInfoString ) );
+
+            string path = match.Groups[ "path" ].Value;
+            int lineNumber = Int32.Parse( match.Groups[ "line" ].Value );
+
+            // Construct the test.
+            TestCase test = new TestCase( name, new Uri( TestExecutor.ExecutorUriString ), exeName );
+            test.CodeFilePath = path;
+            test.LineNumber = lineNumber;
+            return test;
+        }
+
+        private static IEnumerable<TestCase> ParseListing( string exeName, IList<string> lines )
+        {
+            // We manually enumerate through the lines of the output.
+            IEnumerator<string> line = lines.GetEnumerator();
+
+            // The first line should be fixed.
+            if( !line.MoveNext() || line.Current != "All available test cases:" )
+            {
+                throw new Exception( "Unexpected line in catch output: " + line.Current );
+            }
+
+            // Split output to groups of lines related to the same test case.
+            // We detect groups by indentation.
+            var currentGroup = new List<string>();
+            int lastIndent = 0;
+            while( line.MoveNext() )
+            {
+                // A new group begins when indent drops.
+                // If there is no indent, we are at the end.
+                int indent = line.Current.Length - line.Current.TrimStart().Length;
+                if ( indent > 0 && indent < lastIndent )
+                {
+                    // Yield the finished group.
+                    yield return LineGroupToTestCase( exeName, currentGroup );
+
+                    // Begin a new group.
+                    currentGroup = new List<string>();
+                }
+                lastIndent = indent;
+
+                // Add the line to the current group.
+                currentGroup.Add( line.Current );
+            }
+
+            // Yield the final group.
+            yield return LineGroupToTestCase( exeName, currentGroup );
         }
     }
 }
