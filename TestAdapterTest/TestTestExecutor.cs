55c0a8af82e6d76f8019765b48364f93462dbf94
Oski Kervinen <oski.kervinen@m-files.com>
2018-03-01 11:15:36 +0200

    Test that executor detects failure
    This test fails currently for the XML-based approach

--- TestAdapterTest/TestTestExecutor.cs
+++ TestAdapterTest/TestTestExecutor.cs
@@ -5,6 +5,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CatchTestAdapter;
 
 using TestAdapterTest.Mocks;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 
 namespace TestAdapterTest
 {
@@ -24,6 +26,32 @@ namespace TestAdapterTest
 
             // Make sure we got results for all.
             Assert.AreEqual( Common.ReferenceTestCount, framework.Results.Count );
+        }
+
+        [TestMethod]
+        public void FailureIsFailure()
+        {
+            // Set up a fake testing context.
+            var framework = new MockFrameworkHandle();
+
+            // Execute all tests.
+            TestExecutor executor = new TestExecutor();
+            executor.RunTests( Common.ReferenceExeList, new MockRunContext(), framework );
+
+            // Map the tests by name.
+            Dictionary<string, TestResult> resultsByName = new Dictionary<string, TestResult>();
+            foreach( var result in framework.Results )
+            {
+                resultsByName[ result.TestCase.FullyQualifiedName ] = result;
+            }
+
+            // Check that the failure failed and success succeeded.
+            TestResult failure = resultsByName[ "Has failure" ];
+            Assert.AreEqual( TestOutcome.Failed, failure.Outcome );
+            Assert.IsTrue( failure.ErrorStackTrace.Contains( "33" ) ); // Failure line number.
+
+            TestResult success = resultsByName[ "With tags" ];
+            Assert.AreEqual( TestOutcome.Passed, success.Outcome );
         }
     }
 }
