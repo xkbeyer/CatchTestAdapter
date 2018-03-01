1a77aa42d88dcdf98d09572ec6b175d0f4b49529
Oski Kervinen <oski.kervinen@m-files.com>
2018-03-01 11:01:56 +0200

    Initial test for the test executor

---
+++ TestAdapterTest/Mocks/MockRunContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAdapterTest.Mocks
+{
+    class MockRunContext : IRunContext
+    {
+        public bool KeepAlive => true;
+
+        public bool InIsolation => false;
+
+        public bool IsDataCollectionEnabled => false;
+
+        public bool IsBeingDebugged => System.Diagnostics.Debugger.IsAttached;
+
+        public string TestRunDirectory => throw new NotImplementedException();
+
+        public string SolutionDirectory => throw new NotImplementedException();
+
+        public IRunSettings RunSettings => throw new NotImplementedException();
+
+        public ITestCaseFilterExpression GetTestCaseFilter( IEnumerable<string> supportedProperties, Func<string, TestProperty> propertyProvider )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TestAdapterTest/TestTestExecutor.cs
+++ TestAdapterTest/TestTestExecutor.cs
@@ -9,14 +9,21 @@ using TestAdapterTest.Mocks;
 namespace TestAdapterTest
 {
     [TestClass]
+    [DeploymentItem( Common.ReferenceExePath )]
     public class TestTestExecutor
     {
         [TestMethod]
         public void TestExecutesAllTests()
         {
+            // Set up a fake testing context.
+            var framework = new MockFrameworkHandle();
 
+            // Execute all tests.
             TestExecutor executor = new TestExecutor();
-            
+            executor.RunTests( Common.ReferenceExeList, new MockRunContext(), framework );
+
+            // Make sure we got results for all.
+            Assert.AreEqual( Common.ReferenceTestCount, framework.Results.Count );
         }
     }
 }
