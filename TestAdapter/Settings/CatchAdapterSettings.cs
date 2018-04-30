63cde1f21ad14e81840d0b1501af741565cf34b4
Oski Kervinen <oski.kervinen@m-files.com>
2018-04-30 10:01:19 +0300

    Add setting to filter test executables
    Currently, the adapter treats all executables as test executables. This is very inconvenient when the solution includes other executables, which get run on every test discovery round.
    
    Implemented a setting for filtering the tests. Since this is the first setting for the adapter, had to initialize the infrastructure for reading settings from the runsettings file.

---
+++ TestAdapter/Settings/CatchAdapterSettings.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+
+namespace TestAdapter.Settings
+{
+    /// <summary>
+    /// Settings for the catch test adapter.
+    /// Found as the element "Catch" in a Visual Studio runsettings xml file.
+    /// </summary>
+    [XmlRoot(XmlRoot)]
+    class CatchAdapterSettings : TestRunSettings
+    {
+        /// <summary>
+        /// The name of the XML that holds these settings.
+        /// </summary>
+        public const string XmlRoot = "Catch";
+
+        public CatchAdapterSettings(): base( XmlRoot ) { }
+
+        #region Settings
+
+        /// <summary>
+        /// Regex used to find test executables.
+        /// </summary>
+        [DefaultValue( @".*\.exe" )]
+        public string TestExeFilter { get; set; } = @".*\.exe";
+
+        #endregion
+
+        #region Interpretations
+
+        /// <summary>
+        /// Returns true if the given executable should be treated as a source of tests.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public bool IncludeTestExe( string name )
+        {
+            return Regex.Match( name, this.TestExeFilter ).Success;
+        }
+
+        #endregion Interpretations
+
+        public override XmlElement ToXml()
+        {
+            // Create a temporary Xml document.
+            XmlDocument doc = new XmlDocument();
+
+            // Write the settings to it.
+            using ( var writer = doc.CreateNavigator().AppendChild() )
+            {
+                var serializer = new XmlSerializer( typeof( CatchAdapterSettings ) );
+                serializer.Serialize( writer, this );
+            }
+
+            // Because the element is a root in the temporary document,
+            // some root specific elements were automatically added to it,
+            // but it won't actually be a root where we want it, so we remove them.
+            doc.DocumentElement.RemoveAllAttributes();
+
+            return doc.DocumentElement;
+        }
+    }
+}
---
+++ TestAdapter/Settings/CatchSettingsProvider.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using System.Xml.Serialization;
+
+namespace TestAdapter.Settings
+{
+    /// <summary>
+    /// SettingsProvider used to register our interest in the "Catch" node.
+    /// </summary>
+    [Export( typeof( ISettingsProvider ) )]
+    [SettingsName( CatchAdapterSettings.XmlRoot )]
+    class CatchSettingsProvider : ISettingsProvider
+    {
+        /// <summary>
+        /// Stored the last loaded settings, if any.
+        /// </summary>
+        public CatchAdapterSettings Settings { get; set; }
+
+        /// <summary>
+        /// Load the settings from xml.
+        /// </summary>
+        /// <param name="reader">Reader pointed at the root node of the adapter settings node.</param>
+        public void Load( XmlReader reader )
+        {
+            // Check that the node is correct.
+            if(reader.Read() && reader.Name == CatchAdapterSettings.XmlRoot )
+            {
+                // Deserialize the settings.
+                var deserializer = new XmlSerializer( typeof(CatchAdapterSettings) );
+                this.Settings = deserializer.Deserialize( reader ) as CatchAdapterSettings;
+            }
+        }
+
+        /// <summary>
+        /// Loads settings from the runsettings in the discovery context.
+        /// Returns default settings otherwise.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public static CatchAdapterSettings LoadSettings(IDiscoveryContext context )
+        {
+            CatchAdapterSettings settings = new CatchAdapterSettings();
+
+            if( context.RunSettings != null )
+            {
+                var provider = context.RunSettings.GetSettings( CatchAdapterSettings.XmlRoot ) as CatchSettingsProvider;
+                if( provider != null && provider.Settings != null )
+                {
+                    settings = provider.Settings;
+                }
+            }
+
+            return settings;
+        }
+    }
+}
