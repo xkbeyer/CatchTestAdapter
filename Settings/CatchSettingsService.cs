e2ec47151d0b8681191794636b1a3ec10679f225
Oski Kervinen <oski.kervinen@m-files.com>
2018-06-12 15:53:03 +0300

    Create a SettingsService
    I hope it will use dependency injection magic
    to put settings in the correct place.

---
+++ Settings/CatchSettingsService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.XPath;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Microsoft.VisualStudio.TestWindow.Extensibility;
+using TestAdapter.Settings;
+
+namespace CatchTestAdapter.Settings
+{
+    [Export( typeof( IRunSettingsService ) )]
+    [SettingsName( CatchAdapterSettings.XmlRoot )]
+    class CatchSettingsService : IRunSettingsService
+    {
+        public string Name => CatchAdapterSettings.XmlRoot;
+
+        public IXPathNavigable AddRunSettings( IXPathNavigable inputRunSettingDocument, IRunSettingsConfigurationInfo configurationInfo, ILogger log )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
